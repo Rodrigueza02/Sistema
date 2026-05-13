a0608cfe256c80ecd1f4b8e96faca275c1ed290c
Rodrigueza02 <mariaj.rodrigueza@campusucc.edu.co>
2026-05-13 16:21:09 -0500

    se añaden vistas faltantes en academic

--- sistema_academico/academic/views.py
+++ sistema_academico/academic/views.py
@@ -87,3 +87,84 @@ def asistencia_mensual(request):
         'labels': [d['mes'].strftime('%b %Y') for d in data],
         'data':   [d['total'] for d in data],
     })
+    
+    # Agregar estas clases al views.py de academic que ya tenías
+
+class CursoListView(DocenteOAdminMixin, ListView):
+    model = Curso
+    template_name = 'academic/curso_list.html'
+    context_object_name = 'cursos'
+
+class CursoCreateView(SoloAdminMixin, CreateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'academic/curso_form.html'
+    success_url = reverse_lazy('curso-list')
+
+class CursoUpdateView(SoloAdminMixin, UpdateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'academic/curso_form.html'
+    success_url = reverse_lazy('curso-list')
+
+class CursoDeleteView(SoloAdminMixin, DeleteView):
+    model = Curso
+    template_name = 'academic/curso_confirm_delete.html'
+    success_url = reverse_lazy('curso-list')
+
+class NotaListView(LoginRequiredMixin, ListView):
+    model = Nota
+    template_name = 'academic/nota_list.html'
+    context_object_name = 'notas'
+
+    def get_queryset(self):
+        qs = super().get_queryset().select_related('estudiante', 'materia', 'curso')
+        # Los estudiantes solo ven sus propias notas
+        if self.request.user.es_estudiante():
+            qs = qs.filter(estudiante=self.request.user)
+        return qs
+
+class NotaUpdateView(DocenteOAdminMixin, UpdateView):
+    model = Nota
+    form_class = NotaForm
+    template_name = 'academic/nota_form.html'
+    success_url = reverse_lazy('nota-list')
+
+class NotaDeleteView(SoloAdminMixin, DeleteView):
+    model = Nota
+    template_name = 'academic/nota_confirm_delete.html'
+    success_url = reverse_lazy('nota-list')
+
+class AsistenciaListView(DocenteOAdminMixin, ListView):
+    model = Asistencia
+    template_name = 'academic/asistencia_list.html'
+    context_object_name = 'asistencias'
+
+class AsistenciaCreateView(DocenteOAdminMixin, CreateView):
+    model = Asistencia
+    form_class = AsistenciaForm
+    template_name = 'academic/asistencia_form.html'
+    success_url = reverse_lazy('asistencia-list')
+
+class BuscadorView(LoginRequiredMixin, ListView):
+    template_name = 'academic/buscar.html'
+    context_object_name = 'resultados'
+
+    def get_queryset(self):
+        from django.db.models import Q
+        from accounts.models import CustomUser
+        q = self.request.GET.get('q', '').strip()
+        if not q:
+            return []
+        estudiantes = CustomUser.objects.filter(
+            rol='estudiante'
+        ).filter(
+            Q(first_name__icontains=q) | Q(last_name__icontains=q) | Q(email__icontains=q)
+        )
+        cursos = Curso.objects.filter(Q(nombre__icontains=q) | Q(periodo__icontains=q))
+        return {'estudiantes': estudiantes, 'cursos': cursos, 'query': q}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['query'] = self.request.GET.get('q', '')
+        return ctx

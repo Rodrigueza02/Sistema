74e8f27b7b403fe8f3b9a2470c090c469d768343
HMAlejandra <helen.moncayo@campusucc.edu.co>
2026-05-13 19:54:50 -0500

    feat(accounts): views and admin

--- sistema_academico/academic/admin.py
+++ sistema_academico/academic/admin.py
@@ -1,18 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'rol', 'activo', 'date_joined')
-    list_filter = ('rol', 'activo', 'is_staff', 'is_superuser')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-    ordering = ('-date_joined',)
-    
-    fieldsets = UserAdmin.fieldsets + (
-        ('Información Adicional', {'fields': ('rol', 'activo', 'foto', 'telefono')}),
-    )
-    
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Información Adicional', {'fields': ('rol', 'activo', 'email', 'first_name', 'last_name')}),
-    )
+# Register your models here.
+# Juliana registrará los modelos de Materia, Curso, Nota, Asistencia aquí
